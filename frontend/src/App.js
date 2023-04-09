381aea2cd46d1e052ca589840c5dd3d31bfb2aef
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-04-08 21:37:43 -0400

    root layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,13 +27,20 @@ import EditEventPage from "./pages/EditEvent";
 import EventDetailPage from "./pages/EventDetail";
 import EventsPage from "./pages/Events";
 import NewEventPage from "./pages/NewEvent";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
-	{ path: "/", element: <HomePage /> },
-	{ path: "/events", element: <EventsPage /> },
-	{ path: "/events/:eventId", element: <EventDetailPage /> },
-	{ path: "/events/new", element: <NewEventPage /> },
-	{ path: "/events/:eventId/edit", element: <EditEventPage /> },
+	{
+		path: "/",
+		element: <RootLayout />,
+		children: [
+			{ index: true, element: <HomePage /> },
+			{ path: "events", element: <EventsPage /> },
+			{ path: "events/:eventId", element: <EventDetailPage /> },
+			{ path: "events/new", element: <NewEventPage /> },
+			{ path: "events/:eventId/edit", element: <EditEventPage /> },
+		],
+	},
 ]);
 
 function App() {
