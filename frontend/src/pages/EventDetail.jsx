d195c52ab832a6f79a71d0cefbc52e4012930be2
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-04-08 21:47:20 -0400

    adding links and param id

--- frontend/src/pages/EventDetail.jsx
+++ frontend/src/pages/EventDetail.jsx
@@ -1,5 +1,14 @@
-const EditDetail = () => {
-	return <h1>EditDetail</h1>;
-};
+import { useParams } from "react-router-dom";
 
-export default EditDetail;
+function EventDetailPage() {
+	const params = useParams();
+
+	return (
+		<>
+			<h1>EventDetailPage</h1>
+			<p>Event ID: {params.eventId}</p>
+		</>
+	);
+}
+
+export default EventDetailPage;
--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,5 +1,29 @@
-const Events = () => {
-	return <h1>Events</h1>;
-};
+import { Link } from "react-router-dom";
 
-export default Events;
+const DUMMY_EVENTS = [
+	{
+		id: "e1",
+		title: "Some event",
+	},
+	{
+		id: "e2",
+		title: "Another event",
+	},
+];
+
+function EventsPage() {
+	return (
+		<>
+			<h1>EventsPage</h1>
+			<ul>
+				{DUMMY_EVENTS.map((event) => (
+					<li key={event.id}>
+						<Link to={event.id}>{event.title}</Link>
+					</li>
+				))}
+			</ul>
+		</>
+	);
+}
+
+export default EventsPage;
