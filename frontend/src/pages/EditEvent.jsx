9fb06b894aa9870281c665d606ac856d96f1aa04
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-04-09 19:00:52 -0400

    useRouteLoaderData

--- frontend/src/pages/EditEvent.jsx
+++ frontend/src/pages/EditEvent.jsx
@@ -1,5 +1,11 @@
-const EditEvent = () => {
-	return <h1>EditEvent</h1>;
-};
+import { useRouteLoaderData } from "react-router-dom";
 
-export default EditEvent;
+import EventForm from "../components/EventForm";
+
+function EditEventPage() {
+	const data = useRouteLoaderData("event-detail");
+
+	return <EventForm event={data.event} />;
+}
+
+export default EditEventPage;
