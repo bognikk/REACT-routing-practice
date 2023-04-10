e0ef20be3f03bee90ebc94336c4cc58479810105
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-04-09 22:26:23 -0400

    submitting data programmatically

--- frontend/src/pages/EventDetail.jsx
+++ frontend/src/pages/EventDetail.jsx
@@ -1,4 +1,4 @@
-import { useRouteLoaderData, json } from "react-router-dom";
+import { useRouteLoaderData, json, redirect } from "react-router-dom";
 
 import EventItem from "../components/EventItem";
 
@@ -26,3 +26,22 @@ export async function loader({ request, params }) {
 		return response;
 	}
 }
+
+export const action = async ({ request, params }) => {
+	const eventId = params.eventId;
+
+	const response = await fetch("http://localhost:8080/events/" + eventId, {
+		method: request.method,
+	});
+
+	if (!response.ok) {
+		throw json(
+			{ message: "Could not delete an event." },
+			{
+				status: 500,
+			}
+		);
+	} else {
+		return redirect("/events");
+	}
+};
